c07da670ad786376313ef874d94d9fa8a81ffc9e
fpatten <34715459+fpatten@users.noreply.github.com>
2022-08-29 06:54:58 -0400

    Media Change

--- media/media.lua
+++ media/media.lua
@@ -1,13 +1,29 @@
-local LSM = LibStub("LibSharedMedia-3.0") 
+local LSM = LibStub("LibSharedMedia-3.0")
+
+if LSM == nil then return end
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+
 -- -----
 --   FONT
 -- -----
 LSM:Register ("font", "-FafnyirUI", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-SemiBold.ttf]])
 LSM:Register ("font", "-FafnyirUI_Bold", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-Bold.ttf]])
-LSM:Register ("font", "-Fafnyir_Black", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-Black.ttf]])
+LSM:Register ("font", "-FafnyirUI_Black", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-Black.ttf]])
 LSM:Register ("font", "- BOLTZZ", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\BOLTZZ.ttf]])
+--   SOUND
+-- -----
 
 -- -----
 --   STATUSBAR
 -- -----
 LSM:Register("statusbar", "- FafnyirUI Clean", [[Interface\Addons\SharedMedia_FafnyirUI\statusbar\Clean.tga]])
+LSM:Register("statusbar", "- FafnyirUI Gradient", [[Interface\Addons\SharedMedia_FafnyirUI\statusbar\Gradient.tga]])
+LSM:Register("statusbar", "- FafnyirUI Half", [[Interface\Addons\SharedMedia_FafnyirUI\statusbar\Half.tga]])
